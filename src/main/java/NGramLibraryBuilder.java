01c44dbc14b47077b250f59f575da5fecde2f051
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-04 10:53:07 -0600

    project setup

---
+++ src/main/java/NGramLibraryBuilder.java
@@ -0,0 +1,44 @@
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class NGramLibraryBuilder {
+	public static class NGramMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+		int noGram;
+		@Override
+		public void setup(Context context) {
+
+		}
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			
+
+		}
+	}
+
+	public static class NGramReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		// reduce method
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+
+		}
+	}
+
+}
